9f553bdeeabb7a420b2f7a187d0c2c1188ae9d73
DavidMerchanMartinez <merchandavid1632@gmail.com>
2023-11-27 18:48:07 -0500

    login4 terminado y con responsive

---
+++ js/login4.js
@@ -0,0 +1,42 @@
+const formularioLogin = document.querySelector(".login");
+const formularioRegister = document.querySelector(".register");
+// cambio formulario
+const aLogin = document.querySelector(".a-login");
+const aRegister = document.querySelector(".a-register");
+// cambio label del input
+const inputs =document.querySelectorAll("input");
+const labels =document.querySelectorAll("label");
+
+// eventos de cambio register-login
+aLogin.addEventListener("click", cambioRegister);
+aRegister.addEventListener("click", cambioLogin);
+
+// eventos input
+inputs.forEach(function(input) {
+    input.addEventListener("input", cambioInput);
+});
+
+
+// funciones 
+function cambioRegister(){
+    formularioLogin.classList.add("inactivo");
+    formularioRegister.classList.remove("inactivo");
+}
+
+function cambioLogin(){
+    formularioLogin.classList.remove("inactivo");
+    formularioRegister.classList.add("inactivo");
+}
+
+function cambioInput(){
+    const classList = this.classList;
+    const valor = this.value.trim();
+    const labelAsociado = document.querySelector(`label[for="${classList}"]`);
+
+    if(valor!=""){
+        labelAsociado.classList.add("formulario-activo");
+    }else{
+        labelAsociado.classList.remove("formulario-activo");
+    }
+     
+}
