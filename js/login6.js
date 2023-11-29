691a07bfedefca766816c005086189419e0d0e07
DavidMerchanMartinez <merchandavid1632@gmail.com>
2023-11-29 16:03:56 -0500

    pequños cambios login 2

--- js/login6.js
+++ js/login6.js
@@ -2,15 +2,18 @@
 const formularioRegister = document.querySelector(".register-formulario");
 const formularioLogin = document.querySelector(".login-formulario");
 
-const cambio = document.querySelector(".cambio");
+const cambio = document.querySelector(".cambio-register");
+const cambio2 = document.querySelector(".cambio-login");
 
 cambio.addEventListener("click", cambioFormulario);
+cambio2.addEventListener("click", cambioFormulario);
 
 
-function cambioFormulario(){
-    formularioLogin.classList.toggle("cambio-formulario");
-    cambio.classList.toggle("letra-pequeña");
-    formularioLogin.children.item(0).classList.toggle("letra-pequeña");
-    
+function cambioFormulario(event){
+    if(event.target.classList.item(1)  == "letra-pequeña"){
+        formularioLogin.classList.toggle("cambio-formulario");
+        formularioLogin.children.item(0).classList.toggle("letra-pequeña");
+        formularioRegister.children.item(0).classList.toggle("letra-pequeña");
+    } 
 }
 
